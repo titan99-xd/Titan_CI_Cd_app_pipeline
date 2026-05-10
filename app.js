feacda89923d25e1f8549ee7cd7e1cf7d33d44e9
titan99-xd <abhinavgautam3166@gmail.com>
2026-05-10 15:49:11 +0300

    Fix app.js for testing

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ app.get('/health', (req, res) => {
   res.send('ok')
 })
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+if (process.env.NODE_ENV !== 'test') {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
+}
+
+module.exports = app
